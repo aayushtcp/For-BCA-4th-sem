890291578290b23d22d4326324f5d15fc719ba6a
Aayush <therespawner69@gmail.com>
2024-02-17 19:18:40 +0545

    all running except UPDATE in CRUD

--- Scripting Language/PracticalTwo/CRUD.php
+++ Scripting Language/PracticalTwo/CRUD.php
@@ -37,11 +37,16 @@
     <div class="overlay" id="overlay">
         <form action="" method="post" name="form1">
             <!-- Create or Insert Form -->
-            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="fname" placeholder="fname">
             <br><br>
-            <input type="text" name="phone" placeholder="Phone">
+            <input type="text" name="lname" placeholder="lname">
             <br><br>
-            <input type="text" name="address" placeholder="Address">
+            <input type="email" name="email" placeholder="Email">
+            <br><br>
+            Gender
+            <br>
+            <input name="gender" type="radio" value="M">Male
+            <input name="gender" type="radio" value="F">Female
             <br><br>
             <input type="submit" value="Add Employee">
         </form>
@@ -53,11 +58,16 @@
             <!-- Update Form -->
             <h3>Update form</h3>
             <input type="hidden" name="employee_id" id="employee_id">
-            <input type="text" name="updatename" placeholder="New Name">
+            <input type="text" name="updatefname" placeholder="New First Name">
             <br><br>
-            <input type="text" name="updatephone" placeholder="New Phone">
+            <input type="text" name="updatelname" placeholder="New Last Name">
             <br><br>
-            <input type="text" name="updateaddress" placeholder="New Address">
+            <input type="email" name="updateemail" placeholder="New Email">
+            <br><br>
+            Gender
+            <br>
+            <input type="radio" name="updategender" value="M">Male
+            <input type="radio" name="updategender" value="F">Female
             <br><br>
             <input type="submit" value="Update">
         </form>
@@ -84,17 +94,19 @@
     </script>
     <?php
     $conn = new mysqli("localhost", "root", "", "myemployees");
+
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
     // create a new employee
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['name'])) {
-            $name = $_POST['name'];
-            $phone = $_POST['phone'];
-            $address = $_POST['address'];
-            $sql = mysqli_query($conn, "INSERT INTO employee (name, phone, address) VALUES ('$name', '$phone', '$address')");
+        if (isset($_POST['fname'])) {
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $email = $_POST['email'];
+            $gender = $_POST['gender'];
+            $sql = mysqli_query($conn, "INSERT INTO scriptingLanguage (fname, lname, email, gender) VALUES ('$fname', '$lname', '$email', '$gender')");
             if ($sql) {
                 echo "Employee added successfully";
             } else {
@@ -104,11 +116,11 @@
 
         // update the details
         if (isset($_POST['updatename'])) {
-            $employeeId = $_POST['employee_id'];
-            $updatename = $_POST['updatename'];
-            $updatephone = $_POST['updatephone'];
-            $updateaddress = $_POST['updateaddress'];
-            $sql = mysqli_query($conn, "UPDATE employee SET name='$updatename', phone='$updatephone', address='$updateaddress' WHERE id='$employeeId'");
+            $updatefname = $_POST['updatefname'];
+            $updatelname = $_POST['updatelname'];
+            $updateemail = $_POST['updateemail'];
+            $updategender = $_POST['updategender'];
+            $sql = mysqli_query($conn, "UPDATE scriptingLanguage SET fname='$updatefname', lname='$updatelname', email='$updateemail' WHERE id='$employeeId'");
             if ($sql) {
                 echo "Employee updated successfully";
             } else {
@@ -120,7 +132,7 @@
     // delete system
     if (isset($_GET['delete_id'])) {
         $deleteId = $_GET['delete_id'];
-        $sql = mysqli_query($conn, "DELETE FROM employee WHERE id='$deleteId'");
+        $sql = mysqli_query($conn, "DELETE FROM scriptingLanguage WHERE id='$deleteId'");
         if ($sql) {
             echo "Employee deleted successfully";
         } else {
@@ -129,18 +141,21 @@
     }
 
     // read system
-    $result = mysqli_query($conn, "SELECT * FROM employee");
+    $result = mysqli_query($conn, "SELECT * FROM scriptingLanguage");
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $employeeId = $row['id'];
-            echo "<br>Name: " . $row['name'] . ", Phone: " . $row['phone'] . ", Address: " . $row['address'] . " - ";
+            echo "<br>First Name: " . $row['fname'] . ", Last Name: " . $row['lname'] . ", Email: " . $row['email'] . ", Gender: " . $row['gender'] . " - ";
             echo "<button onClick='openPopup2($employeeId)'>Edit</button>";
             echo " <a href='?delete_id=$employeeId'>Delete</a>";
         }
     } else {
         echo "No employees found";
     }
+
+    $conn->close();
     ?>
+
 </body>
 
 </html>
