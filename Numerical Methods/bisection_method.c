83624e4d738dcee6e76af46cc9cf2c1baec4f18a
Aayush <therespawner69@gmail.com>
2024-05-15 12:53:06 +0545

    bisection method easy solve updated0

--- Numerical Methods/bisection_method.c
+++ Numerical Methods/bisection_method.c
@@ -1,47 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-#define f(x) cos(x) - x *exp(x)
 
-int main()
-{
-    float x0, x1, x2, f0, f1, f2, e;
-    int step = 1;
-/* Inputs */
-up:
-    printf("\nEnter two initial guesses:\n");
-    scanf("%f%f", &x0, &x1);
-    printf("Enter tolerable error: (For ex: 0.001 for 3 decimal places)\n");
-    scanf("%f", &e);
-    /* Calculating Functional Value */
-    f0 = f(x0);
-    f1 = f(x1);
-    /* Checking whether given guesses brackets the root or not. */
-    if (f0 * f1 > 0.0)
-    {
-        printf("Incorrect Initial Guesses.\n");
-        goto up;
-    }
-    /* Implementing Bisection Method */
-    printf("\nStep\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
-    do
-    {
-        x2 = (x0 + x1) / 2;
-        f2 = f(x2);
+double func(double x) {
+    return cos(x) - x * exp(x);
+}
 
-        printf("%d\t\t%f\t%f\t%f\t%f\n", step, x0, x1, x2, f2);
+double bisection(double a, double b, double tol) {
+    double c;
+    while ((b - a) >= tol) {
+        c = (a + b) / 2;
 
-        if (f0 * f2 < 0)
-        {
-            x1 = x2;
-            f1 = f2;
-        }
+        if (func(c) == 0.0)
+            break;
+        else if (func(c) * func(a) < 0)
+            b = c;
         else
-        {
-            x0 = x2;
-            f0 = f2;
-        }
-        step = step + 1;
-    } while (fabs(f2) > e);
-    printf("\nRoot is: %f", x2);
+            a = c;
+    }
+    return c;
+}
+
+int main() {
+    double a = 1, b = 3;
+    double tol = 0.0001;
+    printf("Root: %lf\n", bisection(a, b, tol));
     return 0;
 }
